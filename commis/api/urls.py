3193fd47b22f98bbe18ef8723544d3223598ff2e
Noah Kantrowitz <noah@coderanger.net>
2011-02-13 23:12:23 -0800

    Stub out all the API methods and hook up client_get

--- commis/api/urls.py
+++ commis/api/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import url, patterns
 from commis.api import views
 
 urlpatterns = patterns('',
-    url('^/?', views.list, name='client-list'),
+    url('^(?:/(?P<name>.*))?', views.client, name='client'),
 )
