82ed9d6952da45395f3bedb43603bf79ad87f896
Noah Kantrowitz <noah@coderanger.net>
2011-02-13 23:08:58 -0800

    Use my JSON encoder.

--- commis/api/decorators.py
+++ commis/api/decorators.py
@@ -2,7 +2,6 @@ import base64
 import datetime
 import functools
 import itertools
-import json
 
 from chef.auth import sha1_base64, canonical_request
 from chef.rsa import SSLError
@@ -12,6 +11,7 @@ from django.http import HttpResponse
 from commis.api import conf
 from commis.api.exceptions import ChefAPIError
 from commis.api.models import Client
+from commis.utils import json
 
 def decode_timestamp(request):
     timestamp = request.META.get('HTTP_X_OPS_TIMESTAMP')
