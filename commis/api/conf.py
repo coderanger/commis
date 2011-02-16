f7f05b03bb5e0170d1eb8cab7b616fc8d5a2bfff
Noah Kantrowitz <noah@coderanger.net>
2011-02-15 22:34:41 -0800

    Add a setting for where to store files.

--- commis/api/conf.py
+++ commis/api/conf.py
@@ -1,5 +1,9 @@
+import os
+
 from django.conf import settings
 
 COMMIS_TIME_SKEW = getattr(settings, 'COMMIS_TIME_SKEW', 15*60)
 
 COMMIS_VALIDATOR_NAME = getattr(settings, 'COMMIS_VALIDATOR_NAME', 'validator')
+
+COMMIS_FILE_ROOT = getattr(settings, 'COMMIS_FILE_ROOT', os.path.join(settings.MEDIA_ROOT, 'sandbox'))
