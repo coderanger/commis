52c6a205af2abbb6395c17c91bc54e7bb96a5f4a
Noah Kantrowitz <noah@coderanger.net>
2011-03-14 18:27:13 -0400

    Clean up sandbox URLs.

--- commis/api/urls.py
+++ commis/api/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls.defaults import url, patterns, include
 
 from commis.api.views import client
-from commis.api.sandbox.views import sandbox
 
 urlpatterns = patterns('',
     url(r'^clients(?:/(?P<name>.*))?', client, name='client'),
-    url(r'^sandboxes/(?P<sandbox_id>[^/]*)/(?P<checksum>[^/]*)', sandbox),
-    url(r'^sandboxes/(?P<sandbox_id>[^/]*)', sandbox),
-    url(r'^sandboxes', sandbox),
+    url(r'^sandboxes', include('commis.api.sandbox.urls')),
     url(r'^cookbooks', include('commis.api.cookbook.urls')),
     url(r'^nodes', include('commis.api.node.urls')),
     url(r'^roles', include('commis.api.role.urls')),
