d7573c90080207ae1188fa03e6dbe099098e3b6b
Noah Kantrowitz <noah@coderanger.net>
2011-03-19 21:14:22 -0700

    Stub out Node views and add a real template for index.

--- commis/webui/node/urls.py
+++ commis/webui/node/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls.defaults import patterns, url
 from commis.webui.node import views
 
 urlpatterns = patterns('',
-    url(r'', views.index, name='commis-webui-node-index'),
+    url(r'^new/$', views.create, name='commis-webui-node-create'),
+    url(r'^(?P<name>[^/]+)/delete/$', views.delete, name='commis-webui-node-delete'),
+    url(r'^(?P<name>[^/]+)/edit/$', views.edit, name='commis-webui-node-edit'),
+    url(r'^(?P<name>[^/]+)/$', views.show, name='commis-webui-node-show'),
+    url(r'^$', views.index, name='commis-webui-node-index'),
 )
--- commis/webui/node/views.py
+++ commis/webui/node/views.py
@@ -1,4 +1,25 @@
 from django.template.response import TemplateResponse
 
+from commis.api.node.models import Node
+
 def index(request):
-    return TemplateResponse(request, 'commis/node/index.html', {})
+    return TemplateResponse(request, 'commis/node/index.html', {
+        'opts': Node._meta,
+        'objects': Node.objects.all(),
+    })
+
+
+def create(request):
+    pass
+
+
+def show(request, name):
+    pass
+
+
+def edit(request, name):
+    pass
+
+
+def delete(request, name):
+    pass
