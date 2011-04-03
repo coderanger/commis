10b1cdeb2b9410cdad8b2166af07d84774a493ca
Noah Kantrowitz <noah@coderanger.net>
2011-04-03 16:23:46 -0700

    Generic the search API.

--- commis/search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-
-from commis.search.views import search
-
-urlpatterns = patterns('',
-    url(r'^/(?P<name>[^/]+)', search, name='search_get'),
-    url(r'', search, name='search_list'),
-)
--- commis/urls.py
+++ commis/urls.py
@@ -4,11 +4,13 @@ from commis.cookbook.views import CookbookAPIView
 from commis.data_bag.views import DataBagAPIView
 from commis.node.views import NodeAPIView
 from commis.role.views import RoleAPIView
+from commis.search.views import SearchAPIView
 
 urlpatterns = patterns('',
     url(r'^api/cookbooks', include(CookbookAPIView.as_view())),
     url(r'^api/data', include(DataBagAPIView.as_view())),
     url(r'^api/nodes', include(NodeAPIView.as_view())),
     url(r'^api/roles', include(RoleAPIView.as_view())),
+    url(r'^api/search', include(SearchAPIView.as_view())),
     url(r'', include('commis.webui.urls')),
 )
