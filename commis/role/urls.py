862e168eec862eba7dc551007b243939f71dd300
Noah Kantrowitz <noah@coderanger.net>
2011-04-03 13:31:40 -0700

    Remove legacy role API views.

--- commis/role/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-
-from commis.role.views import role
-
-urlpatterns = patterns('',
-    url(r'^/(?P<name>[^/]*)', role, name='role_get'),
-    url(r'', role, name='role_list'),
-)
--- commis/role/views.py
+++ commis/role/views.py
@@ -1,12 +1,6 @@
-from django.core.urlresolvers import reverse
-from django.http import Http404, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
-from commis.api.decorators import chef_api
 from commis.exceptions import ChefAPIError
 from commis.generic_views import CommisAPIView, api
 from commis.role.models import Role
-from commis.utils import json
 
 class RoleAPIView(CommisAPIView):
     model = Role
@@ -27,64 +21,3 @@ class RoleAPIView(CommisAPIView):
             raise ChefAPIError(404, 'Role %s not found', name)
         role.delete()
         return role
-
-
-@chef_api()
-def role_list(request):
-    data = {}
-    for role in Role.objects.all():
-        data[role.name] = request.build_absolute_uri(reverse('role_get', args=[role.name]))
-    return data
-
-@chef_api(admin=True)
-def role_create(request):
-    if Role.objects.filter(name=request.json['name']).exists():
-        raise ChefAPIError(409, 'Role %s already exists', request.json['name'])
-    role = Role.objects.from_dict(request.json)
-    data = {'uri': request.build_absolute_uri(reverse('role_get', args=[role.name]))}
-    return HttpResponse(json.dumps(data), status=201)
-
-
-@chef_api()
-def role_get(request, name):
-    try:
-        role = Role.objects.get(name=name)
-    except Role.DoesNotExist:
-        raise ChefAPIError(404, 'Role %s not found', name)
-    return role
-
-
-@chef_api(admin=True)
-def role_update(request, name):
-    if request.json['name'] != name:
-        raise ChefAPIError(500, 'Name mismatch')
-    if not Role.objects.filter(name=name).exists():
-        raise ChefAPIError(404, 'Role %s not found', name)
-    return Role.objects.from_dict(request.json)
-
-
-@chef_api(admin=True)
-def role_delete(request, name):
-    try:
-        role = Role.objects.get(name=name)
-    except Role.DoesNotExist:
-        raise ChefAPIError(404, 'Role %s not found', name)
-    role.delete()
-    return role
-
-
-@csrf_exempt
-def role(request, name=None):
-    if not name:
-        if request.method == 'GET':
-            return role_list(request)
-        if request.method == 'POST':
-            return role_create(request)
-    else:
-        if request.method == 'GET':
-            return role_get(request, name)
-        if request.method == 'PUT':
-            return role_update(request, name)
-        if request.method == 'DELETE':
-            return role_delete(request, name)
-    raise Http404
