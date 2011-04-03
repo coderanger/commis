7f7236c2a5dfa5f696d67c626b53320bfb8e965a
Noah Kantrowitz <noah@coderanger.net>
2011-04-03 13:49:21 -0700

    First pass on node API using generic view.

--- commis/urls.py
+++ commis/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, url, include
 
+from commis.node.views import NodeAPIView
 from commis.role.views import RoleAPIView
 
 urlpatterns = patterns('',
+    url(r'^api/nodes', include(NodeAPIView.as_view())),
     url(r'^api/roles', include(RoleAPIView.as_view())),
     url(r'', include('commis.webui.urls')),
 )
