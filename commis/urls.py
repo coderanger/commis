ddc1aa32f8645d0697b261d5dc877e85a9d34fc8
Noah Kantrowitz <noah@coderanger.net>
2011-04-03 13:32:07 -0700

    Install new roles API view.

--- commis/urls.py
+++ commis/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url, include
 
+from commis.role.views import RoleAPIView
+
 urlpatterns = patterns('',
-    url(r'^api/', include('commis.api.urls')),
+    url(r'^api/roles', include(RoleAPIView.as_view())),
     url(r'', include('commis.webui.urls')),
 )
