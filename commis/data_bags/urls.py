40b1ec5cd99a6bacbabca5f7d5604c400721d050
Noah Kantrowitz <noah@coderanger.net>
2011-04-04 13:00:43 -0700

    Forgot to delete data_bag/urls.py when I generic'd it.

--- commis/data_bags/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-
-from commis.data_bag.views import data_bag
-
-urlpatterns = patterns('',
-    url(r'^/(?P<bag_name>[^/]+)/(?P<name>.+)', data_bag, name='data_bag_item_get'),
-    url(r'^/(?P<name>[^/]+)', data_bag, name='data_bag_get'),
-    url(r'', data_bag, name='data_bag_list'),
-)
