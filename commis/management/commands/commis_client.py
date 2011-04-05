b410defc361c977d4a68bd23a8792f429386b3ea
Noah Kantrowitz <noah@coderanger.net>
2011-04-04 17:53:55 -0700

    Rework the generate_validator mgmt command into a generic one for any client.
    Old behavior is still available with --validator.

--- commis/management/commands/commis_client.py
+++ commis/management/commands/commis_client.py
@@ -1,17 +1,21 @@
 import os
+import sys
 from optparse import make_option
 
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 
 from commis import conf
 
-class Command(NoArgsCommand):
-    help = 'Generate a validator client'
+class Command(BaseCommand):
+    help = 'Generate a Chef API client'
 
-    option_list = NoArgsCommand.option_list + (
+    option_list = BaseCommand.option_list + (
         make_option('-o', '--output',
-            default='validator.pem',
-            help='Path to write validator key.'),
+            help='Path to write key.'),
+        make_option('--validator', action='store_true', default=False,
+            help='Generate a validator key'),
+        make_option('-a', '--admin', action='store_true', default=False,
+            help='Generate an admin client'),
         make_option('--noinput', action='store_false', dest='interactive', default=True,
             help='Tells Django to NOT prompt the user for input of any kind.'),
         make_option('--force', action='store_true', default=False,
@@ -37,10 +41,27 @@ class Command(NoArgsCommand):
         else:
             return False
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         from commis.clients.models import Client
         output = options.get('output')
-        name = conf.COMMIS_VALIDATOR_NAME
+        if options.get('validator'):
+            if args:
+                sys.stderr.write('Error: --validator is mutually exclusive with name argument\n')
+                return 1
+            if options.get('admin'):
+                sys.stderr.write('Error: --validator is mutually exclusive with --admin\n')
+                return 1
+            name = conf.COMMIS_VALIDATOR_NAME
+            default_output = 'validator.pem'
+        elif args:
+            name = args[0]
+            default_output = name + '.pem'
+        else:
+            sys.stderr.write('Error: No name specified\n')
+            return 1
+
+        if not output:
+            output = default_output
 
         qs = Client.objects.filter(name=name)
         if qs.exists():
@@ -52,7 +73,7 @@ class Command(NoArgsCommand):
             if not self.ask('A file %s already exists. Would you like to overwrite it?'%output, options):
                 return
 
-        client = Client.objects.create(name=name)
+        client = Client.objects.create(name=name, admin=bool(options.get('admin')))
         if output == '-':
             outf = self.stdout
         else:
