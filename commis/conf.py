631433d1cf69cf2a377c0eecd87f97fcf3250cd8
Jeff Forcier <jeff@bitprophet.org>
2012-05-25 13:52:59 -0700

    Set default validation client name to match 0.10 chef-client

--- commis/conf.py
+++ commis/conf.py
@@ -4,6 +4,6 @@ from django.conf import settings
 
 COMMIS_TIME_SKEW = getattr(settings, 'COMMIS_TIME_SKEW', 15*60)
 
-COMMIS_VALIDATOR_NAME = getattr(settings, 'COMMIS_VALIDATOR_NAME', 'validator')
+COMMIS_VALIDATOR_NAME = getattr(settings, 'COMMIS_VALIDATOR_NAME', 'chef-validator')
 
 COMMIS_FILE_ROOT = getattr(settings, 'COMMIS_FILE_ROOT', os.path.join(settings.MEDIA_ROOT, 'sandbox'))
